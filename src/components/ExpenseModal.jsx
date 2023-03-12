90789b6c3b721ab1498eed97b63b93da21fac726
Pat <50007415+PatrickMediodia@users.noreply.github.com>
2023-03-13 00:14:14 +0800

    Separated option tag for placeholder dropdown value

--- src/components/ExpenseModal.jsx
+++ src/components/ExpenseModal.jsx
@@ -18,7 +18,6 @@ const modalStyle = {
 
 //list of categories
 const categories = [
-  "Please select a category.",
   "Leisure",
   "School",
   "Personal"
@@ -26,122 +25,128 @@ const categories = [
 
 //list of types
 const types = [
-  "Please select a type.",
   "Inflow",
   "Outflow"
 ];
 
-function ExpenseModal({ 
-  title, 
-  modalState, 
-  modalCloseFunction, 
-  formState, 
-  postFunction, 
-  id, 
-  message 
+function ExpenseModal({
+  title,
+  modalState,
+  modalCloseFunction,
+  formState,
+  postFunction,
+  id,
+  message
 }) {
   const [formData, setFormData] = useState(() => formState);
-  useEffect(()=> {
+  useEffect(() => {
     setFormData(formState);
   }, [formState]);
 
-  const functionToExecute = async() => {
+  const functionToExecute = async () => {
     await postFunction(id, formData);
   };
   
   const handleInputChange = (e) => {
-    setFormData(prevState => ({ 
-      ...prevState, 
-      [e.target.name]: e.target.value 
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
     }));
   }
 
   return (
-      <Modal
-        isOpen={modalState}
-        onRequestClose={modalCloseFunction}
-        style={modalStyle}
-        contentLabel="Example Modal"
-        appElement={document.getElementById('root')}
-      >
-        <div className="modal">
-          <div className='modal-header'>
-            <h2>{title}</h2>
-            <img 
-              src={CloseIcon} 
-              className="close-icon" 
-              onClick={modalCloseFunction}
-            />
-          </div>
-          <hr/>
+    <Modal
+      isOpen={modalState}
+      onRequestClose={modalCloseFunction}
+      style={modalStyle}
+      contentLabel="Example Modal"
+      appElement={document.getElementById('root')}
+    >
+      <div className="modal">
+        <div className='modal-header'>
+          <h2>{title}</h2>
+          <img
+            src={CloseIcon}
+            className="close-icon"
+            onClick={modalCloseFunction}
+          />
+        </div>
+        <hr />
 
-          <form className="expense-form">
-            <label class="form-title">Title:</label>
-            <input 
-              className="input-field" 
-              name="title" 
-              id="title"
-              value={formData.title}
-              onChange={handleInputChange}
-            />
-            <label className="form-title">Price:</label>
-            <input 
-              className="input-field" 
-              name="price" 
-              id="price"
-              type="number"
-              value={formData.price}
-              onChange={(event) => {
-                setFormData(prevData => ({
-                  ...prevData,
-                  price: parseFloat(event.target.value)
-                }));
-              }}
-            />
-            <label className="form-title">Date:</label>
-            <DatePicker 
-              name="date"
-              className="input-field" 
-              selected={formData.date}
-              onChange={(date) => { 
-                setFormData(prevData => ({
-                  ...prevData,
-                  date: date
-                }));
-              }}
-            />
-            <label className="form-title">Category:</label>
-            <select 
-              className="dropdown" 
-              name="category"
-              value={formData.category}
-              onChange={handleInputChange}
-            >
-              { categories.map(category => <option value={category}>{category}</option>) }
-            </select>
-            <label className="form-title">Type:</label>
-            <select 
-              className="dropdown" 
-              name="type"
-              value={formData.type}
-              onChange={handleInputChange}
-            >
-              { types.map(type => <option value={type}>{type}</option>) }
-            </select>
-          </form>
+        <form className="expense-form">
+          <label class="form-title">Title:</label>
+          <input
+            className="input-field"
+            name="title"
+            id="title"
+            value={formData.title}
+            onChange={handleInputChange}
+            required
+          />
+          <label className="form-title">Price:</label>
+          <input
+            className="input-field"
+            name="price"
+            id="price"
+            type="number"
+            value={formData.price}
+            onChange={(event) => {
+              setFormData(prevData => ({
+                ...prevData,
+                price: parseFloat(event.target.value)
+              }));
+            }}
+            required
+          />
+          <label className="form-title">Date:</label>
+          <DatePicker
+            name="date"
+            className="input-field"
+            selected={formData.date}
+            onChange={(date) => {
+              setFormData(prevData => ({
+                ...prevData,
+                date: date
+              }));
+            }}
+            required
+          />
+          <label className="form-title">Category:</label>
+          <select
+            className="dropdown"
+            name="category"
+            value={formData.category}
+            onChange={handleInputChange}
+            required
+          >
+            <option value="">Please select a category.</option>
+            {categories.map(category => <option value={category}>{category}</option>)}
+          </select>
+          <label className="form-title">Type:</label>
+          <select
+            className="dropdown"
+            name="type"
+            value={formData.type}
+            onChange={handleInputChange}
+            required
+          >
+            <option value="">Please select a type.</option>
+            {types.map(type => <option value={type}>{type}</option>)}
+          </select>
           <div className="center-button">
-            <Button 
-              method={()=> {
+            <Button
+              method={() => {
                 functionToExecute();
                 modalCloseFunction();
-                alert(message)
+                alert(message);
               }}
               text={title}
               color="#4CAF50"
             />
           </div>
-        </div>
-      </Modal> 
+        </form>
+      </div>
+    </Modal>
   );
 }
 
